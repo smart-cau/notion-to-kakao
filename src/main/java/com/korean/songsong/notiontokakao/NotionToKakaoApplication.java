e85e99655340df15f3e4aa4eade2b10584148dc5
Jung Hyun Woo <1995hyunwoo@naver.com>
2024-01-14 13:36:41 +0000

    create spring project

---
+++ src/main/java/com/korean/songsong/notiontokakao/NotionToKakaoApplication.java
@@ -0,0 +1,13 @@
+package com.korean.songsong.notiontokakao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotionToKakaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotionToKakaoApplication.class, args);
+	}
+
+}
